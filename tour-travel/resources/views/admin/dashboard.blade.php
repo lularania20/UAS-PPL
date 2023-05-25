28aaec140fc491ff091efbc3f716a8574d8ee250
salsabillalula <salsabillalula@gmail.com>
2023-05-26 00:48:37 +0700

    konten-dashboard

--- tour-travel/resources/views/admin/dashboard.blade.php
+++ tour-travel/resources/views/admin/dashboard.blade.php
@@ -9,3 +9,59 @@
         </ol>
     </div>
 @endsection
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title"> <b>Akumulasi Data</b></h3>
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+            </div>
+        </div>
+        <div class="card-body">
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-warning">
+                                <div class="inner">
+                                    <h3>44</h3>
+                                    <p>Wisata</p>
+                                </div>
+                                    <a href="/admin/wisata" class="small-box-footer">Detail<i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-warning">
+                                <div class="inner">
+                                    <h3>44</h3>
+                                    <p>Paket Wisata</p>
+                                </div>
+                                    <a href="/admin/paket-wisata" class="small-box-footer">Detail<i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-warning">
+                                <div class="inner">
+                                    <h3>44</h3>
+                                    <p>Pelanggan</p>
+                                </div>
+                                    <a href="/admin/pelanggan" class="small-box-footer">Detail<i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-warning">
+                                <div class="inner">
+                                    <h3>44</h3>
+                                    <p>Transaksi</p>
+                                </div>
+                                    <a href="/admin/transaksi" class="small-box-footer">Detail<i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+@endsection
+
