bd7d5cfdb252e35576272e7788af23bf85f463bd
jihanghaniyyah <jghaniyyah@gmail.com>
2023-05-25 23:56:17 +0700

    feat: login header

---
+++ tour-travel/resources/views/layouts/header.blade.php
@@ -0,0 +1,9 @@
+<nav class="main-header navbar navbar-expand ml-0">
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                <i class="fas fa-expand-arrows-alt text-white"></i>
+            </a>
+        </li>
+    </ul>
+</nav>
