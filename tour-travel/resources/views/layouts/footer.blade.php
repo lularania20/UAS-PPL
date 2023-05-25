87c67ad63a4367468e69d5b2bae6f91c0fa9c2b0
jihanghaniyyah <jghaniyyah@gmail.com>
2023-05-25 23:56:43 +0700

    fix: admin footer

--- tour-travel/resources/views/layouts/footer.blade.php
+++ tour-travel/resources/views/layouts/footer.blade.php
@@ -1,7 +1,3 @@
 <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-        {{-- <b>Version</b> 3.1.0 --}}
-    </div>
-    {{-- <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved. --}}
-    <strong>Copyright &copy; 2022 <a href="/"> Politeknik Elektronika Negeri Surabaya </a> .</strong> All rights reserved.
+    <span>Copyright &copy; 2023 <a href="/admin"> Tour and Travel </a> .</span> All rights reserved.
 </footer>
