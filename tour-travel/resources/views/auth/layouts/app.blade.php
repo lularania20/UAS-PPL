d2f97298842ccf6c9ffefdf01f7a51f820fde08a
jihanghaniyyah <jghaniyyah@gmail.com>
2023-05-26 10:21:05 +0700

    fix: delete mandated comments

--- tour-travel/resources/views/auth/layouts/app.blade.php
+++ tour-travel/resources/views/auth/layouts/app.blade.php
@@ -8,22 +8,17 @@
     {{-- <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
 
-    {{-- <!-- CSRF Token --> --}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title')</title>
     <link rel="icon" href="{{ asset('assets/img/pens.png') }}" type="image/png">
 
-    {{-- <!-- Scripts --> --}}
     {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
     <script src="{{ asset('js/main.js') }}"></script>
    
-
-    {{-- <!-- Fonts --> --}}
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    {{-- <!-- Styles --> --}}
     <link href="{{ asset('css/stylesign.css') }}" rel="stylesheet">
     {{-- <link rel="stylesheet" href="{{ asset('fontawesome-free/css/all.min.css') }}"> --}}
     {{-- <link rel="stylesheet" href="fontawesome-free/css/all.min.css"> --}}
@@ -48,14 +43,7 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
