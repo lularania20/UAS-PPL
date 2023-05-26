c570732e6b138ce811f2e490d98f1c06876002d1
jihanghaniyyah <jghaniyyah@gmail.com>
2023-05-26 09:56:03 +0700

    fix: logout redirect

--- tour-travel/app/Http/Controllers/Auth/LoginController.php
+++ tour-travel/app/Http/Controllers/Auth/LoginController.php
@@ -44,6 +44,6 @@ class LoginController extends Controller
         request()->session()->invalidate();
         request()->session()->regenerateToken();
 
-        return redirect()->route('admin.login');
+        return redirect()->route('login');
     }
 }
